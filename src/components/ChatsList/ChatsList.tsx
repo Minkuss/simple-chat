637544afc3e775b294480120e809efbc6abd0fbc
Nikita Shijan <nikusia0804@gmail.com>
2023-06-18 02:29:50 +1000

    fixed chats list

--- src/components/ChatsList/ChatsList.tsx
+++ src/components/ChatsList/ChatsList.tsx
@@ -58,85 +58,163 @@ export const ChatsList: FC = () => {
     }
   }, [searchText]);
 
-  useEffect(() => {
-    if (userID) {
-      const doc2 = doc;
-      onSnapshot(doc2(db, "users", userID), (doc) => {
-        doc.data()?.chats.map(async (el: any) => {
-          const chatData: any = (await getDoc(el)).data();
-          onSnapshot(doc2(db, "chats", chatData.id), async (doc) => {
-            // subscribing to chatData changes
-            const chat: any = doc.data();
-            const interlocutorData: any = await (
-              await getDoc(chat.interlocutorUser)
-            ).data();
-            const initiatorData: any = await (
-              await getDoc(chat.initiatorUser)
-            ).data();
-            if (chat.messages.length !== 0) {
-              const lastMessageDate = new Date(
-                chat.messages[chat.messages.length - 1].date.toMillis()
-              );
-              setUserData((prevState) => {
-                const newChats: DocumentData[] = [
-                  ...prevState,
-                  interlocutorData.id !== userID
-                    ? {
-                        photoUrl: interlocutorData.photoUrl,
-                        name: interlocutorData.name,
-                        lastMessage:
-                          chat.messages[chat.messages.length - 1].content,
-                        chatID: chat.id,
-                        id: interlocutorData.id,
-                        date:
-                          String(lastMessageDate.getHours()) +
-                          ":" +
-                          String(lastMessageDate.getMinutes()).padStart(2, "0"),
-                      }
-                    : {
-                        photoUrl: initiatorData.photoUrl,
-                        name: initiatorData.name,
-                        lastMessage:
-                          chat.messages[chat.messages.length - 1].content,
-                        chatID: chat.id,
-                        id: initiatorData.id,
-                        date:
-                          String(lastMessageDate.getHours()) +
-                          ":" +
-                          String(lastMessageDate.getMinutes()).padStart(2, "0"),
-                      },
-                ];
-                return newChats;
-              });
+  // useEffect(() => {
+  //   if (userID) {
+  //     const doc2 = doc;
+  //     onSnapshot(doc2(db, "users", userID), (doc) => {
+  //       doc.data()?.chats.map(async (el: any) => {
+  //         const chatData: any = (await getDoc(el)).data();
+  //         onSnapshot(doc2(db, "chats", chatData.id), async (doc) => {
+  //           // subscribing to chatData changes
+  //           const chat: any = doc.data();
+  //           const interlocutorData: any = await (
+  //             await getDoc(chat.interlocutorUser)
+  //           ).data();
+  //           const initiatorData: any = await (
+  //             await getDoc(chat.initiatorUser)
+  //           ).data();
+  //           if (chat.messages.length !== 0) {
+  //             const lastMessageDate = new Date(
+  //               chat.messages[chat.messages.length - 1].date.toMillis()
+  //             );
+  //             setUserData((prevState) => {
+  //               const newChats: DocumentData[] = [
+  //                 ...prevState,
+  //                 interlocutorData.id !== userID
+  //                   ? {
+  //                       photoUrl: interlocutorData.photoUrl,
+  //                       name: interlocutorData.name,
+  //                       lastMessage:
+  //                         chat.messages[chat.messages.length - 1].content,
+  //                       chatID: chat.id,
+  //                       id: interlocutorData.id,
+  //                       date:
+  //                         String(lastMessageDate.getHours()) +
+  //                         ":" +
+  //                         String(lastMessageDate.getMinutes()).padStart(2, "0"),
+  //                     }
+  //                   : {
+  //                       photoUrl: initiatorData.photoUrl,
+  //                       name: initiatorData.name,
+  //                       lastMessage:
+  //                         chat.messages[chat.messages.length - 1].content,
+  //                       chatID: chat.id,
+  //                       id: initiatorData.id,
+  //                       date:
+  //                         String(lastMessageDate.getHours()) +
+  //                         ":" +
+  //                         String(lastMessageDate.getMinutes()).padStart(2, "0"),
+  //                     },
+  //               ];
+  //               return newChats;
+  //             });
+  //           } else {
+  //             setUserData((prevState) => {
+  //               const newChats: DocumentData[] = [
+  //                 ...prevState,
+  //                 interlocutorData.id !== userID
+  //                   ? {
+  //                       photoUrl: interlocutorData.photoUrl,
+  //                       name: interlocutorData.name,
+  //                       lastMessage: "",
+  //                       chatID: chat.id,
+  //                       id: interlocutorData.id,
+  //                       date: "",
+  //                     }
+  //                   : {
+  //                       photoUrl: initiatorData.photoUrl,
+  //                       name: initiatorData.name,
+  //                       lastMessage: "",
+  //                       chatID: chat.id,
+  //                       id: initiatorData.id,
+  //                       date: "",
+  //                     },
+  //               ];
+  //               return newChats;
+  //             });
+  //           }
+  //         });
+  //       });
+  //     });
+  //   }
+  // }, [userID]);
+
+  const getUserData = useCallback(async () => {
+    const docSnap = await getDoc(doc(db, "users", userID || ""));
+    if (docSnap.exists()) {
+      const dataSnap = docSnap.data();
+      return dataSnap.chats;
+    } else {
+      console.log("Error, no such document");
+    }
+  }, [userID]);
+
+  const getUserChatsData = useCallback(() => {
+    const chatsSnap = getUserData();
+    chatsSnap.then((chatsList) => {
+      chatsList.forEach(async (chatReference: any) => {
+        const chatData: any = (await getDoc(chatReference)).data();
+        onSnapshot(doc(db, "chats", chatData.id), async (doc) => {
+          const chatData: any = doc.data();
+          const interlocutorData: any = (
+            await getDoc(chatData.interlocutorUser)
+          ).data();
+          const initiatorData: any = (
+            await getDoc(chatData.initiatorUser)
+          ).data();
+          const companionData =
+            interlocutorData.id !== userID ? interlocutorData : initiatorData;
+          const lastMessageDate = new Date(
+            chatData.messages.length !== 0
+              ? chatData.messages[chatData.messages.length - 1].date.toMillis()
+              : 0
+          );
+          let newUserData = {
+            photoUrl: companionData.photoUrl,
+            name: companionData.name,
+            lastMessage:
+              chatData.messages.length !== 0
+                ? chatData.messages[chatData.messages.length - 1].content
+                : "",
+            chatID: chatData.id,
+            id: companionData.id,
+            date:
+              chatData.messages.length !== 0
+                ? String(lastMessageDate.getHours()) +
+                  ":" +
+                  String(lastMessageDate.getMinutes()).padStart(2, "0")
+                : "",
+          };
+          setUserData((prevState) => {
+            prevState.forEach((el: DocumentData) => {
+              if (el.name === newUserData.name) {
+                const index = prevState.indexOf(el);
+                prevState[index] = newUserData;
+                newUserData = {
+                  photoUrl: "",
+                  name: "",
+                  lastMessage: "",
+                  chatID: "",
+                  id: "",
+                  date: "",
+                };
+              }
+            });
+            if (newUserData.name !== "") {
+              const newChats: DocumentData[] = [...prevState, newUserData];
+              return newChats;
             } else {
-              setUserData((prevState) => {
-                const newChats: DocumentData[] = [
-                  ...prevState,
-                  interlocutorData.id !== userID
-                    ? {
-                        photoUrl: interlocutorData.photoUrl,
-                        name: interlocutorData.name,
-                        lastMessage: "",
-                        chatID: chat.id,
-                        id: interlocutorData.id,
-                        date: "",
-                      }
-                    : {
-                        photoUrl: initiatorData.photoUrl,
-                        name: initiatorData.name,
-                        lastMessage: "",
-                        chatID: chat.id,
-                        id: initiatorData.id,
-                        date: "",
-                      },
-                ];
-                return newChats;
-              });
+              const newChats: DocumentData[] = [...prevState];
+              return newChats;
             }
           });
         });
       });
-    }
+    });
+  }, [userID]);
+
+  useEffect(() => {
+    getUserChatsData();
   }, [userID]);
 
   return (
